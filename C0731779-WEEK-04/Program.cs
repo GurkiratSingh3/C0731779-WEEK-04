0261d103bb4b8a72c9a502eda3cbeaeb3b745af0
Gurkirat Singh <c0731779@mylambton.ca>
2019-01-30 12:01:50 -0500

    Answer

--- C0731779-WEEK-04/Program.cs
+++ C0731779-WEEK-04/Program.cs
@@ -10,7 +10,8 @@ namespace C0731779_WEEK_04
     {
         static void Main(string[] args)
         {
-   
+            Countryside blue = new Countryside();
+            blue.Launch();
         }
     }
 
@@ -28,17 +29,33 @@ namespace C0731779_WEEK_04
         Village Ajax = new Village();
         Village First;
         Village Last;
-
+        Village Temp;
             public void Launch()
         {
+            Village Temp;
+            First = Maple;
+            Last = Ajax;
             Maple.VillageName = "Maple";
             Maple.nextVillage = Toronto;
             Maple.previousVillage = null;
             Toronto.VillageName = "Toronto";
             Toronto.nextVillage = Ajax;
+            Toronto.nextVillage = Maple;
             Ajax.VillageName = "Ajax";
             Ajax.nextVillage = null;
             Ajax.nextVillage = Toronto;
+
+            Console.WriteLine(this.displayMap());
+            Console.ReadLine();
+        }
+        public string displayMap()
+        {
+            string listofCities = "";       
+            listofCities = listofCities + First.VillageName;
+            Temp = First.nextVillage;
+            listofCities = listofCities + Temp.VillageName;
+            
+            return listofCities;
         }
     }
 }
